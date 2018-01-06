18c5da3c914c6eaaeb6048df22f595e44d2ed9db
顾尧 <gamesgao@163.com>
2018-01-06 15:45:25 +0800

    fixed the bug in index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const logger = require('./lib/logger')('main')
 const config = require('./lib/config')
 const RTMPClient = require('./lib/RTMPClient')
 
-function main () {
+async function main () {
   let rtmpClient = new RTMPClient(config.url, config.key)
   let fileList = fs.readdirSync(config.path)
   while (true) {
     logger.info(`Starting the loop of the path ${config.path}`)
     for (let fileName of fileList) {
-      rtmpClient.push(path.join(config.path, fileName))
+      await rtmpClient.push(path.join(config.path, fileName))
     }
   }
 }

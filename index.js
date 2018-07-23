4fe793d99aaf519f4c76bae2497508c24101ee04
顾尧 <gamesgao@163.com>
2018-07-23 21:42:40 +0800

    Feature: Copy the stream from upcoming one. Can do stream relay.

--- index.js
+++ index.js
@@ -6,14 +6,26 @@ const RTMPClient = require('./lib/RTMPClient')
 
 async function main () {
   let rtmpClient = new RTMPClient(config.url, config.key)
-  while (true) {
-    logger.info(`Starting the loop of the path ${config.path}`)
+  let fileList = fs.readdirSync(config.path)
+  if (config.input) {
     try {
-      await rtmpClient.pushFolder(config.path)
-      // await rtmpClient.push(path.join(config.path, fileName))
+      await rtmpClient.copy(config.input)
     } catch (error) {
       logger.error(`Pushing the video ${config.path} failed`)
       logger.error(error)
+    }
+  } else {
+    while (true) {
+      for (let fileName of fileList) {
+        logger.info(`Starting the loop of the path ${config.path}`)
+        try {
+          // await rtmpClient.pushFolder(config.path)
+          await rtmpClient.push(path.join(config.path, fileName))
+        } catch (error) {
+          logger.error(`Pushing the video ${config.path} failed`)
+          logger.error(error)
+        }
+      }
     }
   }
 }
